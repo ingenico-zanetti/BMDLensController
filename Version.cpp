b7d9f5f16e601b47f4b6c7d6ef9131894366aff7
azanetti <arnaud.zanetti@ingenico.com>
2024-06-20 14:40:49 +0200

    Move FW_VERSION to a separate file

---
+++ Version.cpp
@@ -0,0 +1,1 @@
+const char *const FW_VERSION = "BMD Lens Controller v1.0.2, built on " __DATE__ " at " __TIME__;
---
+++ Version.hpp
@@ -0,0 +1,6 @@
+#ifndef __VERSION_HPP_INCLUDED__
+#define __VERSION_HPP_INCLUDED__
+
+extern const char FW_VERSION[];
+
+#endif // __VERSION_HPP_INCLUDED__
