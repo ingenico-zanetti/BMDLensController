9869277317d28f4a5bad30ea675da7a159a6bb8f
azanetti <arnaud.zanetti@ingenico.com>
2024-06-20 14:58:15 +0200

    Use a function instead of a const variable to export the version string

--- Version.cpp
+++ Version.cpp
@@ -1,1 +1,5 @@
-const char *const FW_VERSION = "BMD Lens Controller v1.0.2, built on " __DATE__ " at " __TIME__;
+static const char *const FW_VERSION = "BMD Lens Controller v1.0.2, built on " __DATE__ " at " __TIME__;
+
+const char *getFWVersion(void){
+  return(FW_VERSION);
+}
--- Version.hpp
+++ Version.hpp
@@ -1,6 +1,6 @@
 #ifndef __VERSION_HPP_INCLUDED__
 #define __VERSION_HPP_INCLUDED__
 
-extern const char FW_VERSION[];
+extern const char *getFWVersion(void);
 
 #endif // __VERSION_HPP_INCLUDED__
